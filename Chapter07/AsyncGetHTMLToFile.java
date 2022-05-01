3704f4c046d1e96fa0545ff00ea706bc304b432a
Thimoty Barbieri <thimoty@thimoty.it>
2022-05-01 16:19:44 +0200

    FIxed url

--- Chapter07/AsyncGetHTMLToFile.java
+++ Chapter07/AsyncGetHTMLToFile.java
@@ -9,7 +9,7 @@ import java.util.concurrent.CompletableFuture;
 class AsyncGetHTMLToFile {
     public static void main(String args[]) throws Exception {
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net        /http/HttpClient.html")).build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/HttpClient.html")).build();
         CompletableFuture<Path> response = client.sendAsync(request, HttpResponse.BodyHandlers.ofFile(Paths.get("http.html"))).thenApply(HttpResponse::body);
         response.get();
     }
